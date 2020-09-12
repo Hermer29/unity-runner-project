ee50822e1143010fcc0f65b5fe70a0f21be98e92
Dmitry <kliman.chan@gmail.com>
2020-09-12 19:37:30 +0300

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PlayerController
+{
+    public class PlayerController : MonoBehaviour
+    {
+        private void Update()
+        {
+            if(Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(-Vector3.right * 0.1f);
+            }
+            if(Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(Vector3.right * 0.1f);
+            }
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                transform.Translate(Vector3.up);
+            }
+        }
+    }
+}
